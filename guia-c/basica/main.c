caa8be6e4e337bea280d9b643322ef9dbcc08d7b
Sasha Berkowsky <snberkowsky@gmail.com>
2025-08-25 15:53:21 -0300

    ej 3

--- guia-c/basica/main.c
+++ guia-c/basica/main.c
@@ -5,7 +5,7 @@ void hello_world() {
     printf("Hello, world!\n");
 }
 
-// 2
+// 3
 void check_size() {
     char c = 100;
     short s = -8712;
@@ -17,17 +17,26 @@ void check_size() {
     unsigned int ui = -12398;
     unsigned long ul = -123812485;
 
+    char* pc = &c;
+    int* pi = &i;
+    void* pv = &l;
+
     printf("--- signed ---\n");
     printf("char(%lu): %d \n", sizeof(c), c);
     printf("short(%lu): %d \n", sizeof(s), s);
     printf("int(%lu): %d \n", sizeof(i), i);
     printf("long(%lu): %ld \n", sizeof(l), l);
 
-    printf("--- unsigned ---\n");
+    printf("\n--- unsigned ---\n");
     printf("char(%lu): %u \n", sizeof(uc), uc);
     printf("short(%lu): %u \n", sizeof(us), us);
     printf("int(%lu): %u \n", sizeof(ui), ui);
     printf("long(%lu): %lu \n", sizeof(ul), ul);
+
+    printf("\n--- pointers ---\n");
+    printf("char(%lu): %p \n", sizeof(pc), pc);
+    printf("int(%lu): %p \n", sizeof(pi), pi);
+    printf("void(%lu): %p \n", sizeof(pv), pv);
 }
 
 int main() {
